51de3d15ca5d152ac56f3418acef8c3494b0aec3
[Luke Mason] <lmason2@zagmail.gonzaga.edu>
2021-04-19 18:11:29 -0700

    Fixed db

--- index.js
+++ index.js
@@ -23,9 +23,8 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-// mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => 
-app.listen(PORT, () => console.log(`server running on port: ${PORT}`));
-    // .catch((error) => console.log(error));
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+    .catch((error) => console.log(error));
 
 mongoose.set("useFindAndModify", false);
